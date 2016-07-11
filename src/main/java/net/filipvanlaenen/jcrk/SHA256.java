62edd6aa98318b01d225fae203ec03feece6e8ad
f.a.vanlaenen@ieee.org <f.a.vanlaenen@ieee.org>
2016-07-11 23:15:49 +0200

    Added SHA-256 as a hash function

---
+++ src/main/java/net/filipvanlaenen/jcrk/SHA256.java
@@ -0,0 +1,40 @@
+/**
+ * jCRK – Cracking cryptographic hash functions implemented in Java.
+ * Copyright © 2016 Filip van Laenen <f.a.vanlaenen@ieee.org>
+ * 
+ * This file is part of jCRK.
+ *
+ * jCRK is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *  
+ * jCRK is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *  
+ * You can find a copy of the GNU General Public License in /doc/gpl.txt
+ * 
+ */
+package net.filipvanlaenen.jcrk;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * The hash function SHA-256. 
+ */
+public final class SHA256 implements HashFunction {
+	private final MessageDigest digest;
+	
+	SHA256() throws NoSuchAlgorithmException {
+		digest = MessageDigest.getInstance("SHA-256");
+	}
+	
+	@Override
+	public byte[] hash(byte[] source) {	
+		return digest.digest(source);
+	}
+
+}
