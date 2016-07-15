066e4a79dc7aa68815dad8bd5e9743a7c0c74a4f
f.a.vanlaenen@ieee.org <f.a.vanlaenen@ieee.org>
2016-07-15 23:24:25 +0200

    Added more unit tests for the compression of InMemorySegmentRepository

--- src/test/java/net/filipvanlaenen/jcrk/InMemorySegmentRepositoryCompressionTest.java
+++ src/test/java/net/filipvanlaenen/jcrk/InMemorySegmentRepositoryCompressionTest.java
@@ -27,6 +27,11 @@ import org.testng.annotations.Test;
  * Unit tests on InMemorySegmentRepository related to the compression method.
  */
 public class InMemorySegmentRepositoryCompressionTest {
+	private static final StandardHashFunction SHA256 = StandardHashFunction.SHA256;
+	private static final Point POINT_00 = new Point((byte) 0x00);
+	private static final Point POINT_01 = new Point((byte) 0x01);
+	private static final Point POINT_FE = new Point((byte) 0xFE);
+	private static final Point POINT_FF = new Point((byte) 0xFF);
 	private SegmentRepository repository;
 
 	/**
@@ -34,9 +39,9 @@ public class InMemorySegmentRepositoryCompressionTest {
 	 */
 	@BeforeMethod
 	public void createNewSegmentRepository() {
-		repository = new InMemorySegmentRepository(StandardHashFunction.SHA256);
+		repository = new InMemorySegmentRepository(SHA256);
 	}
-	
+
 	/**
 	 * Compressing a repository increases its order.
 	 */
@@ -44,7 +49,7 @@ public class InMemorySegmentRepositoryCompressionTest {
 		repository.compressToNextOrder();
 		Assert.assertEquals(repository.getOrder(), 1);
 	}
-	
+
 	/**
 	 * Compressing an empty repository results in an empty repository.
 	 */
@@ -52,5 +57,51 @@ public class InMemorySegmentRepositoryCompressionTest {
 	public void compressionOfEmptyRepositoryProducesEmptyRepository() {
 		repository.compressToNextOrder();
 		Assert.assertTrue(repository.isEmpty());
+	}
+
+	/**
+	 * Compression keeps segments of a higher order.
+	 */
+	@Test
+	public void segmentOfHigherOrderIsKeptDuringCompression() {
+		repository.compressToNextOrder();
+		repository.add(new Segment(POINT_00, POINT_01, 1, 1, SHA256));
+		repository.compressToNextOrder();
+		Assert.assertTrue(repository.contains(new Segment(POINT_00, POINT_01, 1, 2, SHA256)));
+	}
+
+	/**
+	 * Compression doesn't keep lower order segments.
+	 */
+	@Test
+	public void lowerOrderSegmentsAreRemovedDuringCompression() {
+		repository.add(new Segment(POINT_00, POINT_FF, 1, 0, SHA256));
+		repository.add(new Segment(POINT_FF, POINT_00, 1, 0, SHA256));
+		repository.add(new Segment(POINT_FE, POINT_FF, 1, 0, SHA256));
+		repository.compressToNextOrder();
+		Assert.assertTrue(repository.isEmpty());
+	}
+	
+	/**
+	 * Compression combines two lower order segments into a higher segment if possible.
+	 */
+	@Test
+	public void compressionCombinesTwoLowerOrderSegmentsIntoAHigherOrderSegment() {
+		repository.add(new Segment(POINT_00, POINT_FF, 1, 0, SHA256));
+		repository.add(new Segment(POINT_FF, POINT_01, 1, 0, SHA256));
+		repository.compressToNextOrder();
+		Assert.assertTrue(repository.contains(new Segment(POINT_00, POINT_01, 2, 1, SHA256)));
+	}
+
+	/**
+	 * Compression combines three lower order segments into a higher segment if possible.
+	 */
+	@Test
+	public void compressionCombinesThreeLowerOrderSegmentsIntoAHigherOrderSegment() {
+		repository.add(new Segment(POINT_00, POINT_FF, 1, 0, SHA256));
+		repository.add(new Segment(POINT_FF, POINT_FE, 1, 0, SHA256));
+		repository.add(new Segment(POINT_FE, POINT_01, 1, 0, SHA256));
+		repository.compressToNextOrder();
+		Assert.assertTrue(repository.contains(new Segment(POINT_00, POINT_01, 3, 1, SHA256)));
 	}
 }
