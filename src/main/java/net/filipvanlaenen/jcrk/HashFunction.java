28b6e93f11babc3e723b5beca9d22092a472dae6
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2025-03-04 10:34:01 +0100

    Resolved some Checkstyle violations

--- src/main/java/net/filipvanlaenen/jcrk/HashFunction.java
+++ src/main/java/net/filipvanlaenen/jcrk/HashFunction.java
@@ -1,49 +1,28 @@
-/**
- * jCRK – Cracking cryptographic hash functions implemented in Java.
- * Copyright © 2016 Filip van Laenen <f.a.vanlaenen@ieee.org>
- * 
- * This file is part of jCRK.
- *
- * jCRK is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- *  
- * jCRK is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *  
- * You can find a copy of the GNU General Public License in /doc/gpl.txt
- * 
- */
 package net.filipvanlaenen.jcrk;
 
 /**
- * Definition of a hash function. It takes a byte array, and produces a new byte
- * array.
+ * Definition of a hash function. It takes a byte array, and produces a new byte array.
  */
 public interface HashFunction {
-	/**
-	 * The hash function. It takes a byte array, and produces a new byte array.
-	 * 
-	 * @param source
-	 *            The source byte array.
-	 * @return The resulting byte array.
-	 */
-	byte[] hash(byte[] source);
+    /**
+     * The hash function. It takes a byte array, and produces a new byte array.
+     *
+     * @param source The source byte array.
+     * @return The resulting byte array.
+     */
+    byte[] hash(byte[] source);
 
-	/**
-	 * The bit length of the hash function.
-	 * 
-	 * @return The bit length of the hash function.
-	 */
-	int getBitLength();
+    /**
+     * The bit length of the hash function.
+     *
+     * @return The bit length of the hash function.
+     */
+    int getBitLength();
 
-	/**
-	 * The byte length of the hash function.
-	 * 
-	 * @return The byte length of the hash function.
-	 */
-	int getByteLength();
+    /**
+     * The byte length of the hash function.
+     *
+     * @return The byte length of the hash function.
+     */
+    int getByteLength();
 }
